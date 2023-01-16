c269827a6cd91337880d9829d3604d0c8419f0fc
Devynn <personaldevynn@gmail.com>
2023-01-16 10:29:59 -0800

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get('/', (req, res) => {
+    res.send("home page");
+});
+
+app.get('/yo', (req, res) => {
+    res.render("index");
+})
+
+app.listen(port, () => console.log(`Server running at http://localhost:${port}/`))
